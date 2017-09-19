8e91230f9d0a4941bd89b457f76396879b13cab3
Brian Lusina <lusinabrian@gmail.com>
2017-09-19 23:07:57 +0300

    create error 404 page

---
+++ src/app/components/Error404.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * Error404 stateless component
+ */
+const Error404 = () => {
+    return (
+        <div>
+            <h1>Page Not Found</h1>
+            <p>Sorry, but the page you were trying to view does not exist.</p>
+        </div>
+    );
+};
+
+
+export default Error404;
